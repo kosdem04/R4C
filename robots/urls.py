fc6a28c2713eb236b27328364bf35e26a8917521
18.Ctynz,hz.2023 <dementiev.kos04@gmail.com>
2023-10-08 22:27:32 +0700

    task2

--- robots/urls.py
+++ robots/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import robots
+from .views import robots, upload_exel, exel_file
 
 urlpatterns = [
     # Прописываем url-адрес для первого задания
     # Тестовый POST-запрос с информацией в формате JSON будет отправлен в файле test.py
     path('robots/', robots, name='robots'),
+    # Прописываем url-адреса для второго задания
+    # Url-адрес для отображения ссылки для скачивания exel-файла
+    path('exel_file/', exel_file, name='exel_file'),
+    # Url-адрес скачивания самого exel-файла
+    path('exel_file/upload_exel/', upload_exel, name='upload_exel')
 ]
--- robots/views.py
+++ robots/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, FileResponse
 import json
 from django.core.exceptions import ValidationError
 from django.views.decorators.csrf import csrf_exempt
 from .models import Robot
-
+import random
+from openpyxl import Workbook
 
 # Представление для первого задания
 # @csrf_exempt — декоратор в Django, который используется для отключения проверки CSRF
@@ -33,3 +34,40 @@ def robots(request):
     # Обработка GET-запроса
     else:
         return JsonResponse({'message':'Нет активных функций для GET-запроса'})
+
+# Представления для второго задания
+# Вывод ссылки для скачивания Exel-файла
+def exel_file(request):
+    return render(request, 'upload_exel.html')
+
+# Скачивание Exel-файла
+def upload_exel(request):
+    robots = Robot.objects.all()
+    # Определяем информацию, которая будет записана в Exel-файл путём запроса к базе данных и рандомайзера
+    data = []
+    for robot in robots:
+        data += [{'model': robot.model, 'version': robot.version, 'количество за неделю': random.randint(10, 100)}]
+    # Создание новой Exel-файла
+    exel = Workbook()
+    # Создаём для каждой модели свою страницу при помощи множества
+    for model in set([row['model'] for row in data]):
+        # Создаём заголовок для листа
+        sheet = exel.create_sheet(title=f'Модель {model}', index=0)
+        # Прописываем заголовки
+        sheet['A1'] = 'Модель'
+        sheet['B1'] = 'Версия'
+        sheet['C1'] = 'Количество за неделю'
+        # Заполняем лист информацией
+        for i, row in enumerate([row for row in data if row['model'] == model], start=2):
+            sheet.cell(row=i, column=1, value=row['model'])
+            sheet.cell(row=i, column=2, value=row['version'])
+            sheet.cell(row=i, column=3, value=row['количество за неделю'])
+    # Прописываем путь и прописываем автоматическое имя Exel-файла
+    excel_path = f'exel/model_value_for_week{random.randint(10, 100)}.xlsx'
+    # Сохраняем Exel-файл
+    exel.save(excel_path)
+    # Открываем Exel-файл
+    excel_file = open(excel_path, 'rb')
+    # Создаём HTTP-ответ с Exel-файлом в качестве содержимого
+    response = FileResponse(excel_file, as_attachment=True, filename=f'model_value_for_week{random.randint(10, 100)}.xlsx')
+    return response
